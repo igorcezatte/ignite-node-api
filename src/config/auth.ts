deecaa642542d04eb4fab16d4054bbbabe2eb4e4
Igor Cezatte <igorcezatte13@gmail.com>
2021-08-18 23:37:42 -0300

    Refresh Token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: '2db59c78db962c8cf2b6cc2655181c33',
+    expires_in_token: '1d',
+    secret_refresh_token: 'eb155c7e725bb106789115eaecdb500b',
+    expires_in_refresh_token: '30d',
+    expires_refresh_token_days: 30,
+};
